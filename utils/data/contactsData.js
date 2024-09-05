c53e7cdb7b0d033c60895f576dd819974e6f5760
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-09-05 15:39:04 +0000

    update contaacts Data

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -5,7 +5,7 @@ export const contactsData = {
     github: 'https://github.com/mk016',
     facebook: '',
     linkedIn: 'https://www.linkedin.com/in/mahendra-kumawat-59911a253/',
-    twitter: '',
+    twitter: 'https://x.com/Mk__0168',
     stackOverflow: '',
-    devUsername: "mk016"
+    devUsername: "https://dev.to/mk016"
 }
